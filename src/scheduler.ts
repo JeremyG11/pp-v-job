2aa937a1fcae614c0f6a4fb9eb53fffc79a44576
gatwech <gatwech3211@gmail.com>
2024-12-21 09:40:28 +0300

    mooo mooo mooo

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -4,18 +4,20 @@ import { fetchTweetsForAccounts } from "./jobs/fetch-tweet";
 
 // Schedule the job to run periodically
 export const scheduleJobs = () => {
-  // Fetch mentions every 15 minutes
+  // Fetch mentions daily at midnight
   cron.schedule("0 0 * * *", async () => {
     console.log("Fetching mentions...");
     await fetchMentions();
   });
 
-  // Fetch tweets for accounts daily at midnight
-  cron.schedule("0 0 * * *", async () => {
+  // Fetch tweets for accounts daily at 12:05 AM
+  cron.schedule("5 0 * * *", async () => {
     console.log("Running scheduled job: Fetch tweets for accounts");
     await fetchTweetsForAccounts();
     console.log("Completed scheduled job: Fetch tweets for accounts");
   });
 
-  console.log("Tweet fetching job scheduled to run daily at midnight.");
+  console.log(
+    "Jobs scheduled: Fetch mentions at midnight, Fetch tweets at 12:05 AM."
+  );
 };
