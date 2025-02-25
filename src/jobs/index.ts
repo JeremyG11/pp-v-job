e71e21ea66911872fbfa6f6a00de85cb570b14ac
gatwech <gatwech3211@gmail.com>
2025-02-25 17:56:39 +0300

    Rescheduling the time for fetch 15 mins gap .

--- src/jobs/index.ts
+++ src/jobs/index.ts
@@ -13,6 +13,10 @@ import { graph, initializeState } from "@/graphs";
 export const createJobs = (user: User, userTimezone: string): Job[] => [
   {
     id: `fetch-mentions-${user.id}`,
+    /**
+     * Fetch mentions every day at midnight.
+     * This is a cron expression that runs at 12:00 AM every day.
+     */
     schedule: "0 0 * * *",
     handler: async () => {
       await fetchMentions();
@@ -21,15 +25,27 @@ export const createJobs = (user: User, userTimezone: string): Job[] => [
   },
   {
     id: `fetch-tweets-${user.id}`,
-    schedule: "0 5 * * *",
+    /**
+     * Fetch tweets for accounts every day at 12:05 AM.
+     * This is a cron expression that runs at 12:05 AM every day.
+     * This job is scheduled to run after the fetch-mentions job.
+     */
+    schedule: "5 0 * * *",
     handler: async () => {
       await fetchTweetsForAccounts(user.id);
       await generateResponsesForTopTweets(user.id);
     },
     timezone: userTimezone,
   },
+
   {
     id: `fetch-usertimeline-data-${user.id}`,
+    /**
+     * Fetch Twitter data every 60 minutes.
+     * This is a cron expression that runs every 60 minutes.
+     * This job is scheduled to run after the refresh-access-token job.
+     */
+
     schedule: "*/30 * * * *",
     handler: async () => {
       const twitterAccounts = await db.twitterAccount.findMany({
@@ -44,15 +60,21 @@ export const createJobs = (user: User, userTimezone: string): Job[] => [
   },
   {
     id: `fetch-dms-${user.id}`,
+    /**
+     * Fetch DMs every day at 12:20 AM.
+     * This is a cron expression that runs at 12:20 AM every day.
+     * This job is scheduled to run after the fetch-twitter-data job.
+     */
     schedule: "*/30 * * * *",
     handler: async () => {
       // await fetchDMs();
     },
     timezone: userTimezone,
   },
+
   {
     id: `analyze-tweets-${user.id}`,
-    schedule: "25 0 * * *",
+    schedule: "5 0 * * *",
     handler: async () => {
       try {
         const state = await initializeState({ userId: user.id });
